7fb686072d2fd968190f998d6db1d858fca51796
TheQ <thanhquoc23979@gmail.com>
2024-07-27 14:39:22 +0700

    Update

--- Assets/NamAnElements/Scripts/Menu/GamePanel.cs
+++ Assets/NamAnElements/Scripts/Menu/GamePanel.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.SceneManagement;
@@ -9,13 +10,33 @@ using UnityEngine.UI;
 public class GamePanel : MonoBehaviour
 {
     public Button btnPlay;
+    public Slider musicSlider;
+    public Slider soundSlider;
+    public AudioSource audioSource;
+
+
     private void Start()
     {
         btnPlay.onClick.AddListener(OnClickPlay);
+        musicSlider.value = audioSource.volume;
+        soundSlider.value = audioSource.volume;
+
+        musicSlider.onValueChanged.AddListener(SetVolume);
+
+        soundSlider.onValueChanged.AddListener(SetVolume);
     }
+
+    public void SetVolume(float volume)
+    {
+        audioSource.volume = volume;
+    }
+
+
 
     private void OnClickPlay()
     {
         SceneManager.LoadScene("LobbyScene");
     }
+
+
 }
