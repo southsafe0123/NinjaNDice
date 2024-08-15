7bc9c6f685ba6c4655fb901c37c251e77ae4689e
southsafe0123 <naman.nguyen49002@gmail.com>
2024-08-15 10:38:16 +0700

    fix bug

--- Assets/NamAnElements/Scripts/API/LoginPanel/SettingPanel.cs
+++ Assets/NamAnElements/Scripts/API/LoginPanel/SettingPanel.cs
@@ -1,22 +1,51 @@
 ﻿using System;
 using System.Collections;
 using TMPro;
-using Unity.Tutorials.Core.Editor;
 using UnityEngine;
 using UnityEngine.UI;
+using WebSocketSharp;
 
-public class SettingPanel: MonoBehaviour
+public class SettingPanel : MonoBehaviour
 {
     public TextMeshProUGUI txtPlayerName;
     public GameObject btnLogin;
     public GameObject btnLogout;
     public GameObject btnConfirmLogout;
+    public Button btnChangeNameConfirm;
+    public TextMeshProUGUI guestName;
     private void Start()
     {
+        StartCoroutine(SlowUpdateNameCoroutine());
         btnConfirmLogout.GetComponent<Button>().onClick.AddListener(() =>
         {
             StartCoroutine(LogoutCoroutine());
         });
+
+        btnChangeNameConfirm.onClick.AddListener(() =>
+        {
+            if (guestName.text.IsNullOrEmpty()) return;
+            PlayerPrefs.SetString("GuestName",guestName.text);
+        });
+    }
+    private IEnumerator SlowUpdateNameCoroutine()
+    {
+        WaitForSeconds wait015fsecond = new WaitForSeconds(0.15f); 
+        while (true)
+        {
+            yield return null;
+            yield return wait015fsecond;
+            if (UserSessionManager.Instance._id.IsNullOrEmpty())
+            {
+                btnLogin.SetActive(true);
+                btnLogout.SetActive(false);
+            }
+            else
+            {
+                btnLogin.SetActive(false);
+                btnLogout.SetActive(true);
+            }
+            txtPlayerName.text = UserSessionManager.Instance.username.IsNullOrEmpty() ? PlayerPrefs.GetString("GuestName") : UserSessionManager.Instance.username.ToString();
+        }
     }
 
     private IEnumerator LogoutCoroutine()
@@ -29,21 +58,5 @@ public class SettingPanel: MonoBehaviour
         yield return new WaitForSeconds(UnityEngine.Random.Range(0.3f, 1.3f));
         yield return new WaitUntil(() => UserSessionManager.Instance.username.IsNullOrEmpty());
         LoadingPanel.Instance.SetDisplayLoading(false);
-    }
-
-    private void Update()
-    {
-        if (UserSessionManager.Instance._id.IsNullOrEmpty())
-        {
-            btnLogin.SetActive(true);
-            btnLogout.SetActive(false);
-        }
-        else
-        {
-            btnLogin.SetActive(false);
-            btnLogout.SetActive(true);
-        }
-
-        txtPlayerName.text = UserSessionManager.Instance.username.IsNullOrEmpty()? "Guest" : UserSessionManager.Instance.username.ToString();
     }
 }
